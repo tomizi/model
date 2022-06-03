ad25d36e13778be0855548578867e6afd8894486
tomizi <106354750+tomizi@users.noreply.github.com>
2022-06-04 00:30:46 +0200

    Update app1.py

--- LR/app1.py
+++ LR/app1.py
@@ -260,8 +260,8 @@ else:
     rc.plotly_chart(fig,True)
     st.markdown('###')
     st.markdown('###')
-    lc1,rc1 = st.columns((2,2))
     st.header('Prognoza na najbliższe lata:')
+    lc1,rc1 = st.columns((2,2))
     m,r = lc1.number_input('Podaj miesiąc: ',min_value=1,max_value=12,step=1),rc1.number_input('Podaj rok: ',min_value=2022,max_value=2026,step=1)
     
     a = 36
@@ -274,6 +274,6 @@ else:
         for i in t:
             if i[0] == m and i[1] == r:
                 return i[2]
-    st.subheader('Przewidziana ilość sprzedaży w '+str(m)+'-'+str(r)+' to: '+str(model.predict([[szukaj(m,r)]])))
+    st.subheader('Przewidziana ilość sprzedaży w '+str(m)+'-'+str(r)+' to: '+str(model.predict([[szukaj(m,r)][0])))
 
 
