016b895280ee7dd910b8aa999b5cb3c4b10e2488
tomizi <106354750+tomizi@users.noreply.github.com>
2022-06-06 00:37:43 +0200

    Update app1.py

--- LR/app1.py
+++ LR/app1.py
@@ -425,9 +425,9 @@ else:
     ll, rr = st.columns((1,3))
     ll.subheader('Wybierz parametry modelu: ')
     ll.subheader('LOREM IPSUM ......................................................................................... Lorem Ipsum')
-    p = lc.number_input('Wybierz p:',min_value=1,max_value=10,step=1)
-    d = lc.number_input('Wybierz d:',min_value=1,max_value=10,step=1)
-    q = lc.number_input('Wybierz q:',min_value=1,max_value=10,step=1)
+    p = ll.number_input('Wybierz p:',min_value=1,max_value=10,step=1)
+    d = ll.number_input('Wybierz d:',min_value=1,max_value=10,step=1)
+    q = ll.number_input('Wybierz q:',min_value=1,max_value=10,step=1)
     
     ll.markdown('###')
     ll.markdown('###')
