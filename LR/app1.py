79394963bfd1d6236af7fc59e3dd4fe3ee627493
tomizi <106354750+tomizi@users.noreply.github.com>
2022-06-06 14:10:29 +0200

    Update app1.py

--- LR/app1.py
+++ LR/app1.py
@@ -35,8 +35,8 @@ if Model == 'Uzupełnianie danych':
     options=DF['rok'].unique(),
     default=['2019','2021'])
     
-    new_title = '<p style="font-family:sans-serif;text-align: center; color:Blue; font-size: 62px;">Istotni Statystycznie</b>'
-    st.markdown('                '+new_title, unsafe_allow_html=True)
+    new_title = '<b style="font-family:sans-serif;text-align: center; color:Blue; font-size: 62px;">Istotni Statystycznie</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
     st.title(':bar_chart: Model uzupełniający dane dla leków')
     st.header('Regresja wieloraka')
     st.subheader('Jest to metoda pozwalająca szacować wartości danej wielkości za pomocą znanych już wartości innych wielkości. Polega na '+
